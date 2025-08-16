ff0e9f5c2bd0fd03468d0fe050a793bf3ee15a92
Itsyehia <125609890+Itsyehia@users.noreply.github.com>
2025-08-16 15:07:52 +0300

    add k8s

--- flaskapp-database/flaskapp/static/js/signUp.js
+++ flaskapp-database/flaskapp/static/js/signUp.js
@@ -1,15 +1,17 @@
 $(function() {
-	$('#btnSignUp').click(function() {
-		$.ajax({
-			url: '/signUp',
-			data: $('form').serialize(),
-			type: 'POST',
-			sucess: function(response) {
-				console.log(response);
-			},
-			error: function(error) {
-				console.log(error);
-			}
-		});
-	});
+    $('#btnSignUp').click(function(e) {
+        e.preventDefault(); // prevent normal form submit
+
+        $.ajax({
+            url: SIGNUP_URL,   // dynamically injected from template
+            data: $('form').serialize(),
+            type: 'POST',
+            success: function(response) {
+                console.log(response);
+            },
+            error: function(error) {
+                console.log(error);
+            }
+        });
+    });
 });
