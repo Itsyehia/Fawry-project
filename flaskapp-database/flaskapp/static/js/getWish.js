4b5f8e4c232429b241d3c43b382a7fd1c2f3c5d4
Itsyehia <125609890+Itsyehia@users.noreply.github.com>
2025-08-16 16:10:05 +0300

    adjust front end

--- flaskapp-database/flaskapp/static/js/getWish.js
+++ flaskapp-database/flaskapp/static/js/getWish.js
@@ -1,30 +1,32 @@
 $(function() {
-        $.ajax({
-            url: FLASK_URLS.getWish,
-            type: 'GET',
-            success: function(res) {
-                var div = $('<div>')
+    $.ajax({
+        url: FLASK_URLS.getWish,
+        type: 'GET',
+        success: function(res) {
+            try {
+                var wishObj = JSON.parse(res);
+                var divTemplate = $('<div>')
                     .attr('class', 'list-group')
                     .append($('<a>')
                         .attr('class', 'list-group-item active')
-                        .append($('<h4>')
-                            .attr('class', 'list-group-item-heading'),
-                            $('<p>')
-                            .attr('class', 'list-group-item-text')));
-                var wishObj = JSON.parse(res);
+                        .append($('<h4>').attr('class', 'list-group-item-heading'))
+                        .append($('<p>').attr('class', 'list-group-item-text'))
+                    );
 
-                var wish = '';
- 
                 $.each(wishObj, function(index, value) {
-                    wish = $(div).clone();
+                    var wish = divTemplate.clone();
                     $(wish).find('h4').text(value.Title);
                     $(wish).find('p').text(value.Description);
                     $('.jumbotron').append(wish);
                 });
-            },
-            error: function(error) {
-                console.log(error);
+            } catch (e) {
+                console.error('Failed to parse wishes:', e);
                 alert('Failed to load wishes. Please refresh the page.');
             }
-        });
+        },
+        error: function(error) {
+            console.log(error);
+            alert('Failed to load wishes. Please refresh the page.');
+        }
+    });
 });
--- flaskapp-database/flaskapp/static/js/signUp.js
+++ flaskapp-database/flaskapp/static/js/signUp.js
@@ -1,24 +1,28 @@
 $(function() {
-	$('#btnSignUp').click(function() {
-		$.ajax({
-			url: FLASK_URLS.signUp,
-			data: $('form').serialize(),
-			type: 'POST',
-			success: function(response) {
-				console.log(response);
-				var responseObj = JSON.parse(response);
-				if (responseObj.message) {
-					alert('Success: ' + responseObj.message);
-					// Redirect to sign in page
-					window.location.href = FLASK_URLS.showSignIn || '/showSignIn';
-				} else if (responseObj.error) {
-					alert('Error: ' + responseObj.error);
-				}
-			},
-			error: function(error) {
-				console.log(error);
-				alert('An error occurred during sign up. Please try again.');
-			}
-		});
-	});
+    $('#btnSignUp').click(function() {
+        $.ajax({
+            url: FLASK_URLS.signUp,
+            type: 'POST',
+            data: $('form').serialize(),
+            success: function(response) {
+                console.log(response);
+                try {
+                    var responseObj = JSON.parse(response);
+                    if (responseObj.message) {
+                        alert('Success: ' + responseObj.message);
+                        window.location.href = FLASK_URLS.showSignIn;
+                    } else if (responseObj.error) {
+                        alert('Error: ' + responseObj.error);
+                    }
+                } catch (e) {
+                    console.error('Failed to parse response:', e);
+                    alert('Unexpected response from server.');
+                }
+            },
+            error: function(error) {
+                console.log(error);
+                alert('An error occurred during sign up. Please try again.');
+            }
+        });
+    });
 });
