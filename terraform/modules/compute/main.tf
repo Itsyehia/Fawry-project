87cf49ff1c0a95f10e94f537eae7c0e6c0fcbe5c
Itsyehia <125609890+Itsyehia@users.noreply.github.com>
2025-08-21 21:52:08 +0300

    adjust pem key

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -8,18 +8,16 @@ resource "aws_key_pair" "this" {
   public_key = tls_private_key.this.public_key_openssh
 
   lifecycle {
-    ignore_changes = [public_key]
-    create_before_destroy = true
+    ignore_changes         = [public_key]
+    create_before_destroy  = true
   }
 }
 
 resource "local_file" "private_key_pem" {
-  content  = tls_private_key.this.private_key_pem
-  filename = "${path.module}/poc-${var.env}-key.pem"
+  content         = tls_private_key.this.private_key_pem
+  filename        = "${path.module}/poc-${var.env}-key.pem"
   file_permission = "0600"
 }
-
-# Security group is now managed by the network module
 
 data "aws_ami" "ubuntu" {
   most_recent = true
@@ -36,14 +34,13 @@ data "aws_ami" "ubuntu" {
   }
 }
 
-
 resource "aws_instance" "this" {
-  count         = var.instance_count
-  ami = data.aws_ami.ubuntu.id
-  instance_type = "t3.small"
-  subnet_id     = element(var.subnet_ids, count.index)
-  vpc_security_group_ids = [var.k3s_security_group_id]
-  key_name      = aws_key_pair.this.key_name
+  count                       = var.instance_count
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = "t3.small"
+  subnet_id                   = element(var.subnet_ids, count.index)
+  vpc_security_group_ids      = [var.k3s_security_group_id]
+  key_name                    = aws_key_pair.this.key_name
   associate_public_ip_address = true
 
   tags = {
