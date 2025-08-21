c24518713c578294a7e171c886cfb2b9836a2ebe
Itsyehia <125609890+Itsyehia@users.noreply.github.com>
2025-08-21 20:08:47 +0300

    update import

--- flaskapp-database/flaskapp/tests/test_app.py
+++ flaskapp-database/flaskapp/tests/test_app.py
@@ -1,10 +1,7 @@
 import pytest
 from unittest.mock import patch, MagicMock
-from app import (
-    app,
-    inject_base_path,
-    redirect_with_base
-)# remove unused variables and imports in test_app.py to pass the pipeline
+from app import app
+# remove unused variables and imports in test_app.py to pass the pipeline
 # autoflake --in-place --remove-unused-variables
 # --remove-all-unused-imports tests/test_app.py; autopep8 --in-place
 # --aggressive --aggressive tests/test_app.py; flake8 tests/test_app.py
