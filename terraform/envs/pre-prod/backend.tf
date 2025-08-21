5f7f741b0daf62e8bda51b7fc83310b35bf5295b
Itsyehia <125609890+Itsyehia@users.noreply.github.com>
2025-08-21 21:36:32 +0300

    adjust backend

--- terraform/envs/pre-prod/backend.tf
+++ terraform/envs/pre-prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "poc-prod-vpc-logs-123456789012-preprod" # replace with your AWS account ID
+    bucket = "poc-prod-vpc-logs-${ACCOUNT_ID}-preprod"
     key            = "pre-prod/terraform.tfstate"
     region         = "eu-west-1"
     encrypt        = true
