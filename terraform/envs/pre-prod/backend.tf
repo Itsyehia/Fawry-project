e28f2f720acfc7829587f8a28e5b63dee0ae2412
Itsyehia <125609890+Itsyehia@users.noreply.github.com>
2025-08-21 21:42:05 +0300

    adjust pipeline

--- terraform/envs/pre-prod/backend.tf
+++ terraform/envs/pre-prod/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "poc-prod-vpc-logs-${ACCOUNT_ID}-preprod"
+    bucket = "poc-prod-vpc-logs-599192675916-preprod"
     key            = "pre-prod/terraform.tfstate"
     region         = "eu-west-1"
     encrypt        = true
